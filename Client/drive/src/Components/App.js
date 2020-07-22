995a6a1d14d48d40d2ae468a89dbbdb1d3a188d6
Divanshu Sharma <sharma.divanshu30@gmail.com>
2020-07-22 16:19:32 -0700

    Client side code

---
+++ Client/drive/src/Components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import UploadBox from './Upload';
+import './App.css';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <UploadBox />
+            </div>
+        );
+    }
+}
+
+export default App;
+
