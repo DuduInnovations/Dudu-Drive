704a9e975c5777b7d5caa101c0f4625dd1a656e3
Divanshu Sharma <sharma.divanshu30@gmail.com>
2020-07-26 14:02:32 -0700

    Redux setup pushing again

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import UploadBox from './Upload';
 import './App.css';
+import { simpleAction } from '../actions/simpleAction';
+import { connect } from 'react-redux';
+
+
+const mapDispatchToProps = dispatch => ({
+    simpleAction: () => dispatch(simpleAction())
+  })
+  
+  /* 
+   * mapStateToProps
+  */
+const mapStateToProps = state => ({
+    ...state
+  })
 
 class App extends React.Component {
 
+    simpleAction = (event) => {
+        this.props.simpleAction();
+    }
 
     render() {
         return (
             <div>
                 <UploadBox />
+                <pre>
+              {
+                JSON.stringify(this.props)
+              }
+            </pre>
+            <button onClick={this.simpleAction}>Test redux action</button>
             </div>
         );
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Required for Redux store setup
+import { Provider } from 'react-redux'
+import configureStore from './store';
+import registerServiceWorker from './registerServiceWorker';
 import App from './Components/App';
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+    <Provider store={configureStore()}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
+  registerServiceWorker();
